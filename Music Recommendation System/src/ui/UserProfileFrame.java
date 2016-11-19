8da4f3471f485b9f24d2a0605da677c2513d8ee1
Tara Prasad Tripathy <ihm2014003@iiita.ac.in>
2016-11-19 20:33:33 +0530

    Commit with Error

--- Music Recommendation System/src/ui/UserProfileFrame.java
+++ Music Recommendation System/src/ui/UserProfileFrame.java
@@ -29,10 +29,12 @@ public class UserProfileFrame extends JFrame {
 	private JTabbedPane tabbedPane;
 	private JPanel Mainpanel, recommendedSongPane, recommendedArtistPane, allSongPane, allArtistPane, likedSongPane,
 			likedArtistPane;
-	private JScrollPane songScrollPane, artistScrollPane;
-	private JList songList, artistList;
+	private JScrollPane rSongScrollPane, rArtistScrollPane, lSongScrollPane, lArtistScrollPane, aSongScrollPane,
+			aArtistScrollPane;
+	private JList rSongList, rArtistList, lSongList, lArtistList, aSongList, aArtistList;
 	private JButton likeSongButton, refreshSongsButton, likeArtistButton, refreshArtistsButton;
-	DefaultListModel songListModel, artistListModel;
+	DefaultListModel rSongListModel, rArtistListModel, lSongListModel, lArtistListModel, aSongListModel,
+			aArtistListModel;
 
 	public UserProfileFrame(User user, MainController mainController) {
 		this.mainController = mainController;
@@ -45,18 +47,18 @@ public class UserProfileFrame extends JFrame {
 	}
 
 	@SuppressWarnings({ "rawtypes", "unchecked", "unchecked", "serial", "unchecked" })
-	private void displaySongs(JPanel panel) {
-		songListModel = new DefaultListModel();
-		songList = new JList(songListModel);
-		songList.setCellRenderer(new SongListCellRenderer(songList));
-		songScrollPane = new JScrollPane(songList);
+	private void displayRecommendedSongs(JPanel panel) {
+		rSongListModel = new DefaultListModel();
+		rSongList = new JList(rSongListModel);
+		rSongList.setCellRenderer(new SongListCellRenderer(rSongList));
+		rSongScrollPane = new JScrollPane(rSongList);
 		refreshSongsButton = new JButton("Refresh");
 		likeSongButton = new JButton("Like");
 		refreshSongsButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				try {
-					refreshSongList();
+					refreshRecommendedSongList();
 				} catch (SQLException | IOException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
@@ -67,30 +69,30 @@ public class UserProfileFrame extends JFrame {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				try {
-					likeSong(songList.getSelectedValue());
+					likeSong(rSongList.getSelectedValue());
 				} catch (SQLException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
 			}
 		});
-		panel.add(songScrollPane, BorderLayout.CENTER);
+		panel.add(rSongScrollPane, BorderLayout.CENTER);
 		panel.add(likeSongButton, BorderLayout.NORTH);
 		panel.add(refreshSongsButton, BorderLayout.AFTER_LAST_LINE);
 		try {
-			refreshSongList();
+			refreshRecommendedSongList();
 		} catch (SQLException | IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 	}
 
-	private void displayArtists(JPanel panel) {
-		artistScrollPane = new JScrollPane();
-		artistListModel = new DefaultListModel();
-		artistList = new JList(artistListModel);
-		artistList.setCellRenderer(new ArtistListCellRenderer(artistList));
-		artistScrollPane.setViewportView(artistList);
+	private void displayRecommendedArtists(JPanel panel) {
+		rArtistScrollPane = new JScrollPane();
+		rArtistListModel = new DefaultListModel();
+		rArtistList = new JList(rArtistListModel);
+		rArtistList.setCellRenderer(new ArtistListCellRenderer(rArtistList));
+		rArtistScrollPane.setViewportView(rArtistList);
 
 		refreshArtistsButton = new JButton("Refresh");
 		likeArtistButton = new JButton("Like");
@@ -98,7 +100,7 @@ public class UserProfileFrame extends JFrame {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				try {
-					refreshArtistList();
+					refreshRecommendedArtistList();
 				} catch (SQLException | IOException e1) {
 					e1.printStackTrace();
 				}
@@ -108,20 +110,130 @@ public class UserProfileFrame extends JFrame {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				try {
-					likeArtist(artistList.getSelectedValue());
+					likeArtist(rArtistList.getSelectedValue());
 				} catch (SQLException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
 			}
 		});
-		panel.add(artistScrollPane, BorderLayout.CENTER);
+		panel.add(rArtistScrollPane, BorderLayout.CENTER);
 		panel.add(likeArtistButton, BorderLayout.NORTH);
 		panel.add(refreshArtistsButton, BorderLayout.AFTER_LAST_LINE);
 		try {
-			refreshArtistList();
+			refreshRecommendedArtistList();
 		} catch (SQLException | IOException e1) {
 			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
+
+	private void displayAllSongs(JPanel panel) {
+		aSongListModel = new DefaultListModel();
+		aSongList = new JList(aSongListModel);
+		aSongList.setCellRenderer(new SongListCellRenderer(aSongList));
+		aSongScrollPane = new JScrollPane(aSongList);
+		List<Song> songs;
+		try {
+			songs = mainController.getAllSongList();
+			aSongList.setListData(songs.toArray());
+		} catch (IOException | SQLException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
+
+		likeSongButton = new JButton("Like");
+		likeSongButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					likeSong(aSongList.getSelectedValue());
+				} catch (SQLException e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
+		panel.add(aSongScrollPane, BorderLayout.CENTER);
+		panel.add(likeSongButton, BorderLayout.NORTH);
+	}
+
+	private void displayAllArtists(JPanel panel) {
+		aArtistScrollPane = new JScrollPane();
+		aArtistListModel = new DefaultListModel();
+		aArtistList = new JList(aArtistListModel);
+		aArtistList.setCellRenderer(new ArtistListCellRenderer(aArtistList));
+		aArtistScrollPane.setViewportView(aArtistList);
+		List<Artist> artists;
+		try {
+			artists = mainController.getAllArtistList();
+			aArtistList.setListData(artists.toArray());
+		} catch (SQLException | IOException e2) {
+			e2.printStackTrace();
+		}
+
+		likeArtistButton = new JButton("Like");
+		likeArtistButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					likeArtist(aArtistList.getSelectedValue());
+				} catch (SQLException e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
+		panel.add(aArtistScrollPane, BorderLayout.CENTER);
+		panel.add(likeArtistButton, BorderLayout.NORTH);
+	}
+
+	private void displayLikedSongs(JPanel panel) {
+		lSongListModel = new DefaultListModel();
+		lSongList = new JList(lSongListModel);
+		lSongList.setCellRenderer(new SongListCellRenderer(lSongList));
+		lSongScrollPane = new JScrollPane(lSongList);
+		refreshSongsButton = new JButton("Refresh");
+		refreshSongsButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					refreshLikedSongList();
+				} catch (SQLException | IOException e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
+		panel.add(lSongScrollPane, BorderLayout.CENTER);
+		panel.add(refreshSongsButton, BorderLayout.AFTER_LAST_LINE);
+		try {
+			refreshLikedSongList();
+		} catch (SQLException | IOException e1) {
+			e1.printStackTrace();
+		}
+	}
+
+	private void displayLikedArtists(JPanel panel) {
+		lArtistScrollPane = new JScrollPane();
+		lArtistListModel = new DefaultListModel();
+		lArtistList = new JList(lArtistListModel);
+		lArtistList.setCellRenderer(new ArtistListCellRenderer(lArtistList));
+		lArtistScrollPane.setViewportView(lArtistList);
+
+		refreshArtistsButton = new JButton("Refresh");
+		refreshArtistsButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					refreshLikedArtistList();
+				} catch (SQLException | IOException e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
+		panel.add(lArtistScrollPane, BorderLayout.CENTER);
+		panel.add(refreshArtistsButton, BorderLayout.AFTER_LAST_LINE);
+		try {
+			refreshLikedArtistList();
+		} catch (SQLException | IOException e1) {
 			e1.printStackTrace();
 		}
 	}
@@ -144,29 +256,33 @@ public class UserProfileFrame extends JFrame {
 		recommendedSongPane = new JPanel();
 		recommendedSongPane.setLayout(new BorderLayout());
 		recommendedSongPane.setBackground(new Color(102, 51, 153));
-		displaySongs(recommendedSongPane);
+		displayRecommendedSongs(recommendedSongPane);
 
 		recommendedArtistPane = new JPanel();
 		recommendedArtistPane.setLayout(new BorderLayout());
 		recommendedArtistPane.setBackground(new Color(102, 51, 153));
-		displayArtists(recommendedArtistPane);
+		displayRecommendedArtists(recommendedArtistPane);
 
 		allSongPane = new JPanel();
 		allSongPane.setLayout(new BorderLayout());
 		allSongPane.setBackground(new Color(102, 51, 153));
+		displayAllSongs(allSongPane);
 
 		allArtistPane = new JPanel();
 		allArtistPane.setLayout(new BorderLayout());
 		allArtistPane.setBackground(new Color(102, 51, 153));
+		displayAllArtists(allArtistPane);
 
 		likedSongPane = new JPanel();
 		likedSongPane.setLayout(new BorderLayout());
 		likedSongPane.setBackground(new Color(102, 51, 153));
-
+		displayLikedSongs(likedSongPane);
+		
 		likedArtistPane = new JPanel();
 		likedArtistPane.setLayout(new BorderLayout());
 		likedArtistPane.setBackground(new Color(102, 51, 153));
-
+		displayLikedArtists(likedArtistPane);
+		
 		ImageIcon icon = new ImageIcon("images/a.png");
 		tabbedPane.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
 		tabbedPane.addTab("Recommended Songs", icon, recommendedSongPane, "Does nothing");
@@ -182,14 +298,24 @@ public class UserProfileFrame extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
-	void refreshSongList() throws FileNotFoundException, SQLException, IOException {
+	void refreshRecommendedSongList() throws FileNotFoundException, SQLException, IOException {
 		List<Song> songs = mainController.getRecommendedSongList(loggedUser);
-		songList.setListData(songs.toArray());
+		rSongList.setListData(songs.toArray());
 	}
 
-	void refreshArtistList() throws SQLException, FileNotFoundException, IOException {
+	void refreshRecommendedArtistList() throws SQLException, FileNotFoundException, IOException {
 		List<Artist> artists = mainController.getRecommendedArtistList(loggedUser);
-		artistList.setListData(artists.toArray());
+		rArtistList.setListData(artists.toArray());
+	}
+
+	void refreshLikedSongList() throws FileNotFoundException, SQLException, IOException {
+		List<Song> songs = mainController.getLikedSongList(loggedUser);
+		lSongList.setListData(songs.toArray());
+	}
+
+	void refreshLikedArtistList() throws SQLException, FileNotFoundException, IOException {
+		List<Artist> artists = mainController.getLikedArtistList(loggedUser);
+		lArtistList.setListData(artists.toArray());
 	}
 
 	public void likeSong(Object object) throws SQLException {
